9bda9d5523c8e4517540faca30b41235b669e702
manaya sasaki <safe.dust7139@fastmail.com>
2024-08-04 23:11:28 +0900

    init typescirpt && express

---
+++ main.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+const app: express.Express = express()
+const port = 3000
+
+app.get('/', (_req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
